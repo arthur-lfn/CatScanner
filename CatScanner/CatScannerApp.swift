398dee5a426e72e57fee53fba085fd8567789569
Arturo Alfani <aalfani22@fed.idserve.net>
2022-11-16 09:56:33 +0100

    Initial Commit

---
+++ CatScanner/CatScannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CatScannerApp.swift
+//  CatScanner
+//
+//  Created by Arturo Alfani on 16/11/22.
+//
+
+import SwiftUI
+
+@main
+struct CatScannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
